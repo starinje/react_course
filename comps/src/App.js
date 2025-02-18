4a94f60116074228b4a88f469f541290e0af4f64
Jared Starin <jstarin@tql.com>
2025-02-18 15:32:40 -0500

    Dropdown and panel component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [selection, setSelection] = useState(null)
+
+  const handleSelect = (option) => {
+    setSelection(option)
+  }
+
 
   const options = [
     { label: 'Red', value: 'red' },
     { label: 'Green', value: 'green' },
     { label: 'Blue', value: 'blue' },
   ]
-  return <Dropdown options={options}/>;
+  return (
+    <div className="flex">
+      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+      <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    </div>
+  ) 
+  
+  
 }
 
 export default App;
--- comps/src/components/Dropdown.js
+++ comps/src/components/Dropdown.js
@@ -1,5 +1,56 @@
-function Dropdown({ options }) {
-  return <div>Dropdown...</div>
+import {useState, useEffect, useRef } from 'react'
+import { GoChevronDown } from 'react-icons/go'
+import Panel from './Panel'
+
+function Dropdown({ options, value, onChange }) {
+  const [isOpen, setisOpen] = useState(false)
+  const divEl = useRef()
+
+  useEffect(() => {
+    const handler = (event) => {
+      if(!divEl.current.contains(event.target)){
+        setisOpen(false)
+      }
+    }
+
+    document.addEventListener('click', handler, true)
+
+    return () => {
+      document.removeEventListener('click', handler)
+    }
+  }, [])
+
+
+  const handleClick = () => {
+    setisOpen(!isOpen)
+  }
+
+  const handleOptionClick = (option) => {
+    setisOpen(false)
+    onChange(option)
+  }
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div 
+        className="hover:bg-sky-100 rounded cursor-pointer p-1" 
+        onClick={() => handleOptionClick(option)} 
+        key={option.value}
+      >
+        {option.label}
+      </div>
+    )
+  })
+  
+  return (
+    <div ref={divEl} className="w-48 relative">
+      <Panel className="flex justify-between items-center cursor-pointer " onClick={handleClick}>
+        {value?.label || 'Select ...'}
+        <GoChevronDown className="text-lg"/>
+      </Panel>
+      {isOpen && <Panel className="absolute top-full">{renderedOptions}</Panel>}
+    </div>
+  )
 }
 
 export default Dropdown 
